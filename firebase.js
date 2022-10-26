7531758c1920c05332a4348c1834f8cfe1b9105f
Miguel Fernandez <miguelfera@gmail.com>
2022-10-26 16:16:27 -0600

    Add and initialize  next-auth and firebase

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "clones-sites.firebaseapp.com",
+  projectId: "clones-sites",
+  storageBucket: "clones-sites.appspot.com",
+  messagingSenderId: "652376395626",
+  appId: "1:652376395626:web:f1d74db56143ee40725f13",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFireStore();
+const storage = getStorage();
+
+export { app, db, storage };
