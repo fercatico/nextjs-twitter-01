2912c0009ca94b53d47d361428dbd49119ad2a26
Miguel Fernandez <miguelfera@gmail.com>
2022-10-26 11:25:48 -0600

    Add random users section of the Widgets Component

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -2,8 +2,9 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 import News from "./News";
 
-const Widgets = ({ articles }) => {
-  const [articleNumber, setArticleNumber] = useState(3);
+const Widgets = ({ articles, randomUsers }) => {
+  const [articlesNumber, setArticlesNumber] = useState(3);
+  const [usersNumber, setUsersNumber] = useState(3);
 
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
@@ -17,12 +18,38 @@ const Widgets = ({ articles }) => {
           />
         </div>
       </div>
-      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%] ">
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">What's happening?</h4>
-        {articles.slice(0, articleNumber).map((article) => (
+        {articles.slice(0, articlesNumber).map((article) => (
           <News key={article.title} article={article} />
         ))}
-        <button onClick={() => setArticleNumber(articleNumber + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+        <button
+          onClick={() => setArticlesNumber(articlesNumber + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show more...
+        </button>
+      </div>
+      <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        {randomUsers.slice(0,usersNumber).map((user) => (
+          <div key={user.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
+            <img
+              className="rounded-full"
+              width="40"
+              src={user.picture.thumbnail}
+              alt="user-img"
+            />
+            <div className="truncate ml-4 leading-5">
+              <h4 className="font-bold hover:underline text-[14px] truncate">@{user.login.username}</h4>
+              <h5 className="text-[13px] text-gray-500 truncate">{user.name.first + " " + user.name.last}</h5>
+            </div>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
+              Follow
+            </button>
+          </div>
+        ))}
+        <button onClick={() => setUsersNumber(usersNumber + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
           Show more...
         </button>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home({ newsResult }) {
+export default function Home({ newsResult, randomUsersResult }) {
   return (
     <div>
       <Head>
@@ -14,7 +14,10 @@ export default function Home({ newsResult }) {
       <main className="flex min-h-screen mx-auto">
         <Sidebar />
         <Feed />
-        <Widgets articles={newsResult.articles} />
+        <Widgets
+          articles={newsResult.articles}
+          randomUsers={randomUsersResult.results}
+        />
       </main>
     </div>
   );
@@ -23,12 +26,20 @@ export default function Home({ newsResult }) {
 //https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
 
 export async function getServerSideProps() {
+  //Whats Happening
   const newsResult = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
+
+  // Who to follow section
+  const randomUsersResult = await fetch(
+    "https://randomuser.me/api/?results=50&inc=name,login,picture"
+  ).then((res) => res.json());
+
   return {
     props: {
       newsResult,
+      randomUsersResult,
     },
   };
 }
