9c4026748c759cf417052b7974b411cb68e9b9bb
Miguel Fernandez <miguelfera@gmail.com>
2022-10-27 16:28:38 -0600

    Send data to Firebase and add loading effect

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,10 +1,62 @@
-import { FaceSmileIcon, PhotoIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import {
+  FaceSmileIcon,
+  PhotoIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import React, { useRef, useState } from "react";
 import { useSession, signOut } from "next-auth/react";
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const Input = () => {
   const { data: session } = useSession();
-  
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const filePickerRef = useRef(null);
+
+  const sendPost = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      id: session.user.uid,
+      text: input,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+      name: session.user.name,
+      username: session.user.username,
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), { image: downloadURL });
+      });
+    }
+    setInput("");
+    setSelectedFile(null);
+    setLoading(false);
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
   return (
     <>
       {session && (
@@ -21,14 +73,37 @@ const Input = () => {
                 className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
                 rows="2"
                 placeholder="What's happening?"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               ></textarea>
             </div>
+            {selectedFile && (
+              <div className="relative">
+                <XMarkIcon
+                  className="h-7 text-black absolute cursor-pointer shadow-sm shadow-white rounded-full bg-red-200 border-x-2 border-red p-1 m-1"
+                  onClick={() => setSelectedFile(null)}
+                />
+                <img src={selectedFile} className={`${loading && "animate-pulse"}`} alt="" />
+              </div>
+            )}
             <div className="flex items-center justify-between pt-2.5">
               <div className="flex ">
-                <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                <div className="" onClick={() => filePickerRef.current.click()}>
+                  <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                  <input
+                    type="file"
+                    hidden
+                    ref={filePickerRef}
+                    onChange={addImageToPost}
+                  />
+                </div>
                 <FaceSmileIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
               </div>
-              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+              <button
+                onClick={sendPost}
+                disabled={!input.trim() || loading}
+                className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+              >
                 Tweet
               </button>
             </div>
