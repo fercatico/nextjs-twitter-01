bbd89c64b67b80b4c7bd3803534b27581eaf4009
Miguel Fernandez <miguelfera@gmail.com>
2022-10-26 21:04:13 -0600

    Add session and modify sidebar and input components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -12,8 +12,11 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/24/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Sidebar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/* Twitter Logo*/}
@@ -26,30 +29,48 @@ const Sidebar = () => {
       </div>
       {/* Menu */}
       <div className="mt-4 mb-2.5 xl:items-start">
-        <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+          </>
+        )}
       </div>
       {/* Button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
-      {/* Mini Profile */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://miguelfernandezcampos.netlify.app/static/media/profile-me.eb956e776aecf778c968.jpg"
-          alt="user-img"
-          className="h-10 w-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5  hidden xl:inline">
-          <h4>Miguel Fernandez</h4>
-          <p className="text-gray-500">@fercatico</p>
-        </div>
-        <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
+          {/* Mini Profile */}
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              onClick={signOut}
+              src={session.user.image}
+              alt="user-img"
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5  hidden xl:inline">
+              <h4>{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 };
